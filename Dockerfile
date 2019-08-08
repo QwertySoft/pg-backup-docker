a00d99d98f35cbf2bc1b763a30d70af110b0fc06
Matias Pierobon <matias.pierobonm@gmail.com>
2019-08-08 15:51:37 -0300

    finish backup base idea

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,16 @@ FROM alpine:latest
 
 LABEL maintainer="Matias Pieroboón <mpierobon@qwertysoft.io>"
 
-
 RUN apk --no-cache update && \
   apk --no-cache add ca-certificates groff less python3 postgresql-client && \
   pip3 --no-cache-dir install awscli && \
   rm -rf /var/cache/apk/*
 
 RUN mkdir -p /backups
-ADD backup.sh /opt/backup.sh
+ADD backup.sh /usr/bin/backup
+RUN chmod +x /usr/bin/backup
 
+RUN ln -s /usr/bin/backup /etc/periodic/daily
 
 ENTRYPOINT "crond"
 
--- backup.sh
+++ backup.sh
@@ -1,8 +1,33 @@
 #!/bin/sh
 
 NOW=$(date +"%Y-%m-%dT%H:%M:%SZ")
+FILE="/backups/$NOW.sql.gz"
 
 echo "Creating dump of ${POSTGRES_DATABASE} database from ${POSTGRES_HOST}..."
-pg_dump $POSTGRES_HOST_OPTS $POSTGRES_DATABASE | gzip > "/backups/$NOW.sql.gz"
+export PGPASSWORD="$POSTGRES_PASSWORD"
+pg_dump -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER $POSTGRES_DATABASE | gzip > $FILE || exit 3
+echo "SQL backup successfull"
 
-echo "Uploading dump to $S3_BUCKET"
+if [! -z "$BUCKET"] then
+  echo "Uploading dump to $BUCKET"
+  # export AWS_ACCESS_KEY_ID
+  # export AWS_SECRET_ACCESS_KEY
+  # export AWS_DEFAULT_REGION
+  cat $FILE | aws $AWS_ARGS s3 cp - s3://$BUCKET/$PREFIX/${POSTGRES_DATABASE}_${NOW}.sql.gz || exit 2
+  echo "SQL backup uploaded successfully"
+fi
+
+if [! -z "$MAIL_TO"] then
+  echo "Sending mail to $MAIL_TO"
+  echo "Backup of ${POSTGRES_DATABASE} database successfull at ${NOW}" | mail -v \
+    -r $MAIL_FROM \
+    -s "[${POSTGRES_DATABASE}] backup ${NOW}" \
+    -A $FILE \
+    -S smtp="${MAIL_HOST}:${MAIL_PORT}" \
+    -S smtp-use-starttls \
+    -S smtp-auth=login \
+    -S smtp-auth-user=$MAIL_FROM \
+    -S smtp-auth-password=$MAIL_PASSWORD \
+    -S ssl-verify=ignore \
+    $MAIL_TO || exit 5
+fi
